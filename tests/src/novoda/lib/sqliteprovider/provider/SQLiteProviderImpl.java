84288678b28ab80a9a8d0426916d82e392a65f99
Carl-Gustaf Harroch <carl@novoda.com>
2010-11-25 20:43:43 +0000

    wokring on setting up the tests

---
+++ tests/src/novoda/lib/sqliteprovider/provider/SQLiteProviderImpl.java
@@ -0,0 +1,4 @@
+package novoda.lib.sqliteprovider.provider;
+
+public class SQLiteProviderImpl extends SQLiteProvider {
+}
---
+++ tests/src/novoda/lib/sqliteprovider/provider/SQLiteProviderTest.java
@@ -0,0 +1,33 @@
+
+package novoda.lib.sqliteprovider.provider;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.test.ProviderTestCase2;
+
+public class SQLiteProviderTest extends ProviderTestCase2<SQLiteProviderImpl> {
+
+    public SQLiteProviderTest(Class<SQLiteProviderImpl> providerClass, String providerAuthority) {
+        super(providerClass, providerAuthority);
+    }
+
+    public SQLiteProviderTest() {
+        super(SQLiteProviderImpl.class, "novoda.lib.sqliteprovider.provider");
+    }
+
+    @Override
+    public void testAndroidTestCaseSetupProperly() {
+        super.testAndroidTestCaseSetupProperly();
+    }
+
+    public void testQueryShouldSelectTableFromURI() throws Exception {
+//        assertNotNull(getSingleTable().query(Uri.parse("content://uri/test"), null, null, null,
+//                null));
+    }
+
+    private ContentResolver getSingleTable() throws IllegalAccessException, InstantiationException {
+        return newResolverWithContentProviderFromSql(getContext(), "test",
+                SQLiteProviderImpl.class, "uri", "novoda.lib.sqliteprovider.db", 1,
+                "CREATE TABLE test(_id integer);");
+    }
+}
