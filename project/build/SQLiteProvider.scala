2bab1de48aa693995f90810a5bcae29e01eb1428
Carl-Gustaf Harroch <carl@novoda.com>
2010-11-30 18:12:09 +0000

    finally managed an automated build

--- project/build/SQLiteProvider.scala
+++ project/build/SQLiteProvider.scala
@@ -12,26 +12,26 @@ class SQLiteProvider(info: ProjectInfo) extends ParentProject(info) {
   lazy val main  = project(".", "SQLiteProvider", new MainProject(_))
   lazy val tests = project("tests",  "SQLiteProviderTest", new TestProject(_), main)
 
-  class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults {
-    override def outputDirectoryName = "bin"
-    override def dependencyPath = "libs"
-    override def managedDependencyPath = "libs"
-    override def mainSourcePath = "."
-    
-    val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
-    val specs = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
-    val robolectric = "com.xtremelabs" % "robolectric" % "0.9.2" % "test" from "http://pivotal.github.com/robolectric/downloads/robolectric-0.9.2.jar"
-    val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test"
-    val junit = "junit" % "junit" % "4.8.2" % "test"
-    val wikitext = "org.fusesource.wikitext" % "wikitext-core" % "1.1" % "test"
-    val textile = "org.fusesource.wikitext" % "textile-core" % "1.1" % "test"
-    
-    override def testScalaSourcePath = "specs"
+  class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults {            
   }
 
-  class TestProject(info: ProjectInfo) extends AndroidTestProject(info) with Defaults {
-    override def outputDirectoryName = "bin"    
-    override def managedDependencyPath = "libs"
-    override def mainSourcePath = "."
+  class TestProject(info: ProjectInfo) extends AndroidTestProject(info) with Defaults {  
+        
+    val junitInterface = "com.novocode" % "junit-interface" % "0.5" % "test"
+    
+    val robolectric = "com.xtremelabs" % "robolectric" % "0.9.2" % "test" from "http://pivotal.github.com/robolectric/downloads/robolectric-0.9.2.jar"
+    val javaassit = "javassist" % "javassist" %"3.8.0.GA" % "test"
+    val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test"
+    
+    val junit = "junit" % "junit" % "4.8.2" % "test"    
+    
+    val googlemap = Path.fromFile("/opt/android/add-ons/addon_google_apis_google_inc_8/libs/maps.jar")
+    
+    override def unmanagedClasspath = super.unmanagedClasspath +++ googlemap
+    
+    override def includeTest(s: String) = { s.endsWith("Spec") || s.endsWith("LocalTest")}
+    override def testJavaSourcePath = "src"
+    override def testFrameworks = super.testFrameworks ++ List(new TestFramework("com.novocode.junit.JUnitFrameworkNoMarker"))
+    
   }
 }
