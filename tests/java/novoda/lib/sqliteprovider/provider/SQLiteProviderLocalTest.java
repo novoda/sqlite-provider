df2f8836e697a6e98c4844b4b89425f592233198
akshaydashrath <akshaydashrath@gmail.com>
2010-12-03 05:49:38 +0000

    -completed update

--- tests/java/novoda/lib/sqliteprovider/provider/SQLiteProviderLocalTest.java
+++ tests/java/novoda/lib/sqliteprovider/provider/SQLiteProviderLocalTest.java
@@ -27,141 +27,166 @@ import android.net.Uri;
 @RunWith(RobolectricTestRunner.class)
 public class SQLiteProviderLocalTest {
 
-    SQLiteProviderImpl provider;
+	SQLiteProviderImpl provider;
 
-    @Before
-    public void init() {
-        MockitoAnnotations.initMocks(this);
-        provider = new SQLiteProviderImpl();
-    }
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+		provider = new SQLiteProviderImpl();
+	}
 
-    @Mock
-    SQLiteDatabase db;
+	@Mock
+	SQLiteDatabase db;
 
-    @Mock
-    SQLiteQueryBuilder builder;
+	@Mock
+	SQLiteQueryBuilder builder;
 
-    @Test
-    public void testSelectTableFromUri() throws Exception {
-        // Simple directory listing against table test
-        query("test.com/test");
-        verify(builder).setTables("test");
-    }
-    
-    @Test
-    public void testSelectTableItemFromUri() throws Exception {
-        // Single item against table test
-        query("test.com/test/1");
-        verify(builder).setTables("test"); 
-        verify(builder).appendWhere("_id=1");
-    }
+	@Test
+	public void testSelectTableFromUri() throws Exception {
+		// Simple directory listing against table test
+		query("test.com/test");
+		verify(builder).setTables("test");
+	}
 
-    @Test
-    public void testSelectTableForChildren() throws Exception {
-        query("test.com/parent/1/children");
-        verify(builder).setTables("children");
-        verify(builder).appendWhereEscapeString("parent_id=1");
-    }
-    
-  @Test
-    public void testInsertAgainstCorrectTable() throws Exception{    	
-    	Robolectric.bindShadowClass(ShadowContentUris.class);    	
-    	ContentValues cv = new ContentValues();
-    	cv.put("column1", "2erverver");
-    	when(db.insert(anyString(), anyString(), (ContentValues) anyObject())).thenReturn(2L);
-    	insert("test.com/table1",cv);
-    	insert("test.com/parent/1/child", cv);
-    	verify(db).insert(eq("table1"), anyString(), (ContentValues) anyObject());
-    	verify(db).insert(eq("child"), anyString(), (ContentValues) anyObject());
-    }
-    
-    @Test
-    public void testInsertAgainstOneToManyShouldInputCorrectParam() throws Exception {
-    	Robolectric.bindShadowClass(ShadowContentUris.class);
-    	when(db.insert(anyString(), anyString(), (ContentValues) anyObject())).thenReturn(2L);
-    	ContentValues values = new ContentValues();
-    	values.put("test", "test"); 
-    	values.put("parent_id", "1"); 
-    	insert("test.com/parent/1/children", values);
-    	verify(db).insert(eq("children"), anyString(), (ContentValues) anyObject());
-    }
-    
-    @Test
-    public void testDeleteFromSingleTable() throws Exception {
-    	Robolectric.bindShadowClass(ShadowContentUris.class);
-    	when(db.delete(anyString(), anyString(), (String[]) anyObject())).thenReturn(0);
-    	delete("test.com/parent", null, null);
-    	verify(db).delete(eq("parent"), anyString(), (String[])anyObject());
-    }
-    
-    @Test
-    public void testDeleteFromOneToManyTable() throws Exception {
-    	Robolectric.bindShadowClass(ShadowContentUris.class);
-    	when(db.delete(anyString(), anyString(), (String[]) anyObject())).thenReturn(0);
-    	delete("test.com/parent/1/child", null, null);
-    	verify(db).delete(eq("child"), anyString(), (String[])anyObject());
-    }
-    
-    
-    
-    @Test
-    public void testGroupByQuery() throws Exception {
-    	query("test.com/table?groupBy=table");
-    	verify(builder).query((SQLiteDatabase) anyObject(), (String[])anyObject(), anyString(), (String[])anyObject(), eq("table"), anyString(), anyString(), anyString());   	 
-    }
-    
-    @Test
-    public void testHavingQuery() throws Exception {
-    	query("test.com/table?having=g");
-    	verify(builder).query((SQLiteDatabase) anyObject(), (String[])anyObject(), anyString(), (String[])anyObject(), anyString(), eq("g"), anyString(), anyString());
-    }
-    
-    @Test
-    public void testLimitQuery() throws Exception {
-    	query("test.com/table?limit=100");
-    	verify(builder).query((SQLiteDatabase) anyObject(), (String[])anyObject(), anyString(), (String[])anyObject(), anyString(), anyString(), anyString(), eq("100"));
-    }
-    
-    @Implements(ContentUris.class)
+	@Test
+	public void testSelectTableItemFromUri() throws Exception {
+		// Single item against table test
+		query("test.com/test/1");
+		verify(builder).setTables("test"); 
+		verify(builder).appendWhere("_id=1");
+	}
+
+	@Test
+	public void testSelectTableForChildren() throws Exception {
+		query("test.com/parent/1/children");
+		verify(builder).setTables("children");
+		verify(builder).appendWhereEscapeString("parent_id=1");
+	}
+
+	@Test
+	public void testInsertAgainstCorrectTable() throws Exception{    	
+		Robolectric.bindShadowClass(ShadowContentUris.class);    	
+		ContentValues cv = new ContentValues();
+		cv.put("column1", "2erverver");
+		when(db.insert(anyString(), anyString(), (ContentValues) anyObject())).thenReturn(2L);
+		insert("test.com/table1",cv);
+		insert("test.com/parent/1/child", cv);
+		verify(db).insert(eq("table1"), anyString(), (ContentValues) anyObject());
+		verify(db).insert(eq("child"), anyString(), (ContentValues) anyObject());
+	}
+
+	@Test
+	public void testInsertAgainstOneToManyShouldInputCorrectParam() throws Exception {
+		Robolectric.bindShadowClass(ShadowContentUris.class);
+		when(db.insert(anyString(), anyString(), (ContentValues) anyObject())).thenReturn(2L);
+		ContentValues values = new ContentValues();
+		values.put("test", "test"); 
+		values.put("parent_id", "1"); 
+		insert("test.com/parent/1/children", values);
+		verify(db).insert(eq("children"), anyString(), (ContentValues) anyObject());
+	}
+
+	@Test
+	public void testDeleteFromSingleTable() throws Exception {
+		Robolectric.bindShadowClass(ShadowContentUris.class);
+		when(db.delete(anyString(), anyString(), (String[]) anyObject())).thenReturn(0);
+		delete("test.com/parent", null, null);
+		verify(db).delete(eq("parent"), anyString(), (String[])anyObject());
+	}
+	
+	@Test
+	public void testUpdateFromSingleTable() throws Exception {
+		Robolectric.bindShadowClass(ShadowContentUris.class);
+		when(db.update(anyString(), (ContentValues)anyObject(),anyString(), (String[]) anyObject())).thenReturn(2);
+		ContentValues values = new ContentValues();
+		values.put("test", "test");
+		update("test.com/parent",values, null, null);
+		verify(db).update(eq("parent"), (ContentValues)anyObject(),anyString(), (String[])anyObject());
+	}
+	
+	@Test
+	public void testUpdateFromSOneToMany() throws Exception {
+		Robolectric.bindShadowClass(ShadowContentUris.class);
+		when(db.update(anyString(), (ContentValues)anyObject(),anyString(), (String[]) anyObject())).thenReturn(2);
+		ContentValues values = new ContentValues();
+		values.put("test", "test");
+		update("test.com/parent/1/child",values, null, null);
+		verify(db).update(eq("child"), (ContentValues)anyObject(),anyString(), (String[])anyObject());
+	}
+
+	@Test
+	public void testDeleteFromOneToManyTable() throws Exception {
+		Robolectric.bindShadowClass(ShadowContentUris.class);
+		when(db.delete(anyString(), anyString(), (String[]) anyObject())).thenReturn(0);
+		delete("test.com/parent/1/child", null, null);
+		verify(db).delete(eq("child"), anyString(), (String[])anyObject());
+	}
+
+
+
+	@Test
+	public void testGroupByQuery() throws Exception {
+		query("test.com/table?groupBy=table");
+		verify(builder).query((SQLiteDatabase) anyObject(), (String[])anyObject(), anyString(), (String[])anyObject(), eq("table"), anyString(), anyString(), anyString());   	 
+	}
+
+	@Test
+	public void testHavingQuery() throws Exception {
+		query("test.com/table?having=g");
+		verify(builder).query((SQLiteDatabase) anyObject(), (String[])anyObject(), anyString(), (String[])anyObject(), anyString(), eq("g"), anyString(), anyString());
+	}
+
+	@Test
+	public void testLimitQuery() throws Exception {
+		query("test.com/table?limit=100");
+		verify(builder).query((SQLiteDatabase) anyObject(), (String[])anyObject(), anyString(), (String[])anyObject(), anyString(), anyString(), anyString(), eq("100"));
+	}
+
+	@Implements(ContentUris.class)
 	static
-    class ShadowContentUris {
-    	@Implementation
-    	public static Uri withAppendedId(Uri uri, long id) {
-    		return Uri.parse("content://test.com");
-    	}
-    }
-    
-    private void delete(String uri, String where, String[] whereArgs){
-    	provider.delete(Uri.parse("content://" + uri), where, whereArgs);
-    }
-   
-    
-    private void insert(String uri, ContentValues cv){
-    	provider.insert(Uri.parse("content://" + uri), cv);
-    }
-    private void query(String uri) {
-        provider.query(Uri.parse("content://" + uri), null, null, null, null);
-    }
+	class ShadowContentUris {
+		@Implementation
+		public static Uri withAppendedId(Uri uri, long id) {
+			return Uri.parse("content://test.com");
+		}
+	}
 
-    public class SQLiteProviderImpl extends SQLiteProvider {
-        @Override
-        protected SQLiteDatabase getReadableDatabase() {
-            return db;
-        }
-        
-        @Override
+	private void update(String uri, ContentValues initialValues, String selection,
+			String[] selectionArgs){
+		provider.update(Uri.parse("content://" + uri), initialValues, selection, selectionArgs);
+	}
+
+	private void delete(String uri, String where, String[] whereArgs){
+		provider.delete(Uri.parse("content://" + uri), where, whereArgs);
+	}
+
+
+	private void insert(String uri, ContentValues cv){
+		provider.insert(Uri.parse("content://" + uri), cv);
+	}
+	private void query(String uri) {
+		provider.query(Uri.parse("content://" + uri), null, null, null, null);
+	}
+
+	public class SQLiteProviderImpl extends SQLiteProvider {
+		@Override
+		protected SQLiteDatabase getReadableDatabase() {
+			return db;
+		}
+
+		@Override
 		public
-        void notifyUriChange(Uri uri) {
-        }
-        
-        @Override
-        protected SQLiteDatabase getWritableDatabase() {
-            return db;
-        }
+		void notifyUriChange(Uri uri) {
+		}
 
-        @Override
-        SQLiteQueryBuilder getSQLiteQueryBuilder() {
-            return builder;
-        }
-    }
+		@Override
+		protected SQLiteDatabase getWritableDatabase() {
+			return db;
+		}
+
+		@Override
+		SQLiteQueryBuilder getSQLiteQueryBuilder() {
+			return builder;
+		}
+	}
 }
