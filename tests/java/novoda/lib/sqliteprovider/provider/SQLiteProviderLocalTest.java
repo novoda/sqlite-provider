bc75dc378281b005449cc67a123a78038f10e430
kevinmcdonagh <kevin@novoda.com>
2010-12-03 00:37:20 +0000

    Changed the naming arounf the URIUtils tests.

--- tests/java/novoda/lib/sqliteprovider/provider/SQLiteProviderLocalTest.java
+++ tests/java/novoda/lib/sqliteprovider/provider/SQLiteProviderLocalTest.java
@@ -47,7 +47,6 @@ public class SQLiteProviderLocalTest {
         verify(builder).appendWhere("_id=1");
     }
 
-
     @Test
     public void testSelectTableForChildren() throws Exception {
         query("test.com/parent/1/children");
