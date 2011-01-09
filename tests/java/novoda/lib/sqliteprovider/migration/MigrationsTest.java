261c80ce669f9d2ac2db2e5257863931561d7a68
Carl-Gustaf Harroch <carl@novoda.com>
2011-01-09 22:04:23 +0000

    going from file migration to string migration as ASsetManager does not work against files

--- tests/java/novoda/lib/sqliteprovider/migration/MigrationsTest.java
+++ tests/java/novoda/lib/sqliteprovider/migration/MigrationsTest.java
@@ -5,17 +5,31 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.doReturn;
 import novoda.lib.sqliteprovider.util.RoboRunner;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.io.File;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @RunWith(RoboRunner.class)
 public class MigrationsTest {
+
+    @Mock
+    SQLiteDatabase db;
 
     @Before
     public void init() {
@@ -25,41 +39,56 @@ public class MigrationsTest {
     @Test
     public void testShouldInsertOrNotFileCorrectly() {
         Migrations migration = new Migrations();
-        assertTrue("a correct file name can be inserted", migration.add(new File("12_test.sql")));
-        assertTrue("a correct file name can be inserted",
-                migration.add(new File("121321313_test_teste.sql")));
-        assertFalse("a file name without .sql can not be inserted",
-                migration.add(new File("12_test")));
-        assertFalse("a file name without timestamp can not be inserted",
-                migration.add(new File("test.sql")));
-        
+        assertTrue("a correct file name can be inserted", migration.add("12_test.sql"));
+        assertTrue("a correct file name can be inserted", migration.add("121321313_test_teste.sql"));
+        assertFalse("a file name without .sql can not be inserted", migration.add("12_test"));
+        assertFalse("a file name without timestamp can not be inserted", migration.add("test.sql"));
+
         assertThat(migration.getMigrationsFiles().size(), equalTo(2));
     }
 
     @Test
     public void testOrderedOfImport() {
         Migrations migration = new Migrations();
-        migration.add(new File("3_test.sql"));
-        migration.add(new File("1_test.sql"));
-        migration.add(new File("2_test.sql"));
-        migration.add(new File("5_test.sql"));
-        migration.add(new File("0_test.sql"));
-        
+        migration.add("3_test.sql");
+        migration.add("1_test.sql");
+        migration.add("2_test.sql");
+        migration.add("5_test.sql");
+        migration.add("0_test.sql");
+
         assertThat(migration.getMigrationsFiles().size(), equalTo(5));
-        assertThat(migration.getMigrationsFiles().first().getName(), equalTo("0_test.sql"));
-        assertThat(migration.getMigrationsFiles().last().getName(), equalTo("5_test.sql"));
+        assertThat(migration.getMigrationsFiles().first(), equalTo("0_test.sql"));
+        assertThat(migration.getMigrationsFiles().last(), equalTo("5_test.sql"));
     }
-    
+
     @Test
     public void testStartDate() {
         Migrations migration = new Migrations(4);
-        migration.add(new File("3_test.sql"));
-        migration.add(new File("1_test.sql"));
-        migration.add(new File("2_test.sql"));
-        migration.add(new File("5_test.sql"));
-        migration.add(new File("0_test.sql"));
-        
+        migration.add("3_test.sql");
+        migration.add("1_test.sql");
+        migration.add("2_test.sql");
+        migration.add("5_test.sql");
+        migration.add("0_test.sql");
+
         assertThat(migration.getMigrationsFiles().size(), equalTo(1));
-        assertThat(migration.getMigrationsFiles().last().getName(), equalTo("5_test.sql"));
+        assertThat(migration.getMigrationsFiles().last(), equalTo("5_test.sql"));
+    }
+
+    @Test
+    public void setDBVersion() throws IOException {
+        Context context = mock(Context.class);
+        AssetManager manager = mock(AssetManager.class);
+
+        when(context.getAssets()).thenReturn(manager);
+        when(manager.list(anyString())).thenReturn(new String[] {
+                "1234_test.sql", "12345_test.sql", "123456_test.sql"
+        });
+        
+        when(manager.open(anyString())).thenReturn(
+                new ByteArrayInputStream("CREATE TABLE IF NOT EXIST test(id integer);"
+                        .getBytes("UTF-8")));
+        
+        Migrations.migrate(db, context, "sql");
+        verify(db).setVersion(123456);
     }
 }
