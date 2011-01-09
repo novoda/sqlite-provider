ea06a2417771172c033e660e9a13cd0aa2239ec4
Carl-Gustaf Harroch <carl@novoda.com>
2011-01-09 22:04:23 +0000

    going from file migration to string migration as ASsetManager does not work against files

--- src/novoda/lib/sqliteprovider/sqlite/ExtendedSQLiteOpenHelper2.java
+++ src/novoda/lib/sqliteprovider/sqlite/ExtendedSQLiteOpenHelper2.java
@@ -1,6 +1,8 @@
 
 package novoda.lib.sqliteprovider.sqlite;
 
+import novoda.lib.sqliteprovider.migration.Migrations;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
@@ -11,26 +13,33 @@ import java.util.Map;
 
 public class ExtendedSQLiteOpenHelper2 extends SQLiteOpenHelper implements IDatabaseMetaInfo {
 
+    private Migrations migration;
+
+    private Context context;
+
+    static {
+        String SELECT_TABLES_NAME = "SELECT name FROM sqlite_master WHERE type='table';";
+    }
+
     public ExtendedSQLiteOpenHelper2(Context context, String name, CursorFactory factory,
             int version) {
         super(context, name, factory, version);
+        this.context = context;
     }
 
     @Override
     public Map<String, SQLiteType> getColumns(String table) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public List<String> getTables() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public List<String> getForeignTables(String table) {
-        
+
         return null;
     }
 
