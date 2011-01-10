be2a042a7484c42c422005a6c5683606cc737853
Carl-Gustaf Harroch <carl@novoda.com>
2011-01-10 23:01:59 +0000

    making migration work but still have an issue with conversion from long to int which needs to be changed. Furthermore the mapping of tables in a join is working somehow but not fully fully.

---
+++ tests/instrumentation/src/novoda/lib/sqliteprovider/testproviders/PerformanceProvicer.java
@@ -0,0 +1,47 @@
+package novoda.lib.sqliteprovider.testproviders;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class PerformanceProvicer extends ContentProvider {
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean onCreate() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+            String sortOrder) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
