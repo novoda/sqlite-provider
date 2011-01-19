68d3542329675796a3e03d29fd22451ccbf75f0b
Carl-Gustaf Harroch <carl@novoda.com>
2011-01-19 17:32:45 +0000

    upsert working with relationship to constrains value

--- src/novoda/lib/sqliteprovider/provider/SQLiteContentProviderImpl.java
+++ src/novoda/lib/sqliteprovider/provider/SQLiteContentProviderImpl.java
@@ -1,7 +1,8 @@
 
 package novoda.lib.sqliteprovider.provider;
 
-import novoda.lib.sqliteprovider.sqlite.ExtendedSQLiteOpenHelper2;
+import novoda.lib.sqliteprovider.provider.action.InsertHelper;
+import novoda.lib.sqliteprovider.sqlite.ExtendedSQLiteOpenHelper;
 import novoda.lib.sqliteprovider.sqlite.ExtendedSQLiteQueryBuilder;
 import novoda.lib.sqliteprovider.util.Log.Provider;
 import novoda.lib.sqliteprovider.util.UriUtils;
@@ -10,12 +11,10 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
-import android.util.Log;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -34,10 +33,19 @@ public class SQLiteContentProviderImpl extends SQLiteContentProvider {
 
     private static final String EXPAND = "expand";
 
+    private InsertHelper helper;
+
+    @Override
+    public boolean onCreate() {
+        super.onCreate();
+        helper = new InsertHelper((ExtendedSQLiteOpenHelper) getDatabaseHelper());
+        return true;
+    }
+
     @Override
     protected SQLiteOpenHelper getDatabaseHelper(Context context) {
         try {
-            return new ExtendedSQLiteOpenHelper2(context);
+            return new ExtendedSQLiteOpenHelper(context);
         } catch (IOException e) {
             e.printStackTrace();
             throw new IllegalStateException(e.getMessage());
@@ -46,46 +54,7 @@ public class SQLiteContentProviderImpl extends SQLiteContentProvider {
 
     @Override
     protected Uri insertInTransaction(Uri uri, ContentValues values) {
-        ContentValues insertValues = (values != null) ? new ContentValues(values)
-                : new ContentValues();
-
-        if (UriUtils.hasParent(uri)) {
-            if (!insertValues.containsKey(UriUtils.getParentId(uri) + "_id")) {
-                insertValues.put(UriUtils.getParentColumnName(uri) + "_id",
-                        UriUtils.getParentId(uri));
-            }
-        }
-
-        int update = 0;
-        long rowId = 0;
-
-        if (values.containsKey("_id")) {
-            insertValues.remove("_id");
-            insertValues.put("_rid", values.getAsString("_id"));
-            update = getWritableDatabase().update(UriUtils.getItemDirID(uri), insertValues,
-                    "_rid=?", new String[] {
-                        values.getAsString("_id")
-                    });
-
-            if (update != 0) {
-                Cursor cur = getReadableDatabase().query(UriUtils.getItemDirID(uri), new String[] {
-                    "_id"
-                }, "_rid=?", new String[] {
-                    values.getAsString("_id")
-                }, null, null, null);
-                cur.moveToFirst();
-                rowId = cur.getLong(0);
-                cur.close();
-            }
-        } else {
-            Log.w("SQL", "inserting without a _id could have wtf effect for uri " + uri);
-        }
-
-        // Upsert
-        if (update == 0) {
-            rowId = getWritableDatabase().insert(UriUtils.getItemDirID(uri), null, insertValues);
-        }
-
+        long rowId = helper.insert(uri, values);
         if (rowId > 0) {
             Uri newUri = ContentUris.withAppendedId(uri, rowId);
             notifyUriChange(newUri);
@@ -163,7 +132,7 @@ public class SQLiteContentProviderImpl extends SQLiteContentProvider {
 
         if (expands.size() > 0) {
             builder.addInnerJoin(expands.toArray(new String[] {}));
-            ExtendedSQLiteOpenHelper2 helper = (ExtendedSQLiteOpenHelper2) getDatabaseHelper();
+            ExtendedSQLiteOpenHelper helper = (ExtendedSQLiteOpenHelper) getDatabaseHelper();
             autoproj = helper.getProjectionMap(tableName.toString(),
                     expands.toArray(new String[] {}));
             builder.setProjectionMap(autoproj);
--- src/novoda/lib/sqliteprovider/sqlite/ExtendedSQLiteOpenHelper.java
+++ src/novoda/lib/sqliteprovider/sqlite/ExtendedSQLiteOpenHelper.java
@@ -1,161 +1,111 @@
 
 package novoda.lib.sqliteprovider.sqlite;
 
-import org.apache.http.MethodNotSupportedException;
+import novoda.lib.sqliteprovider.migration.Migrations;
+import novoda.lib.sqliteprovider.util.DBUtils;
 
-import novoda.lib.sqliteprovider.util.DatabaseUtils;
-import novoda.rest.database.SQLiteFileParser;
-import novoda.rest.database.SQLiteTableCreator;
-import novoda.rest.database.SQLiteType;
-
+import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
-public class ExtendedSQLiteOpenHelper extends SQLiteOpenHelper {
+// TODO caching?
+public class ExtendedSQLiteOpenHelper extends SQLiteOpenHelper implements IDatabaseMetaInfo {
 
-    private static final String TAG = ExtendedSQLiteOpenHelper.class.getSimpleName();
-
-    private static final String SELECT_TABLES_NAME = "SELECT name FROM sqlite_master WHERE type='table';";
-
-    private List<String> createdTable = new ArrayList<String>();
-
-    private Map<String, SQLiteTableCreator> createStatements = new HashMap<String, SQLiteTableCreator>();
-
-    private static int dbVersion = 3;
+    private static final String MIGRATIONS_PATH = "migrations";
 
     private Context context;
 
-    public ExtendedSQLiteOpenHelper(Context context) {
-        super(context, new StringBuilder(context.getPackageName()).append(".db").toString(), null,
-                dbVersion);
-        this.context = context;
-        init();
+    /*
+     * We cache the constrains.
+     */
+    private Map<String, List<String>> constrains = new HashMap<String, List<String>>();
+
+    public ExtendedSQLiteOpenHelper(Context context) throws IOException {
+        this(context, context.getPackageName() + ".db", null, Migrations.getVersion(
+                context.getAssets(), MIGRATIONS_PATH));
     }
 
-    protected void init() {
-        final Cursor cur = getReadableDatabase().rawQuery(SELECT_TABLES_NAME, null);
-        while (cur.moveToNext()) {
-            createdTable.add(cur.getString(0));
-        }
-        cur.close();
+    public ExtendedSQLiteOpenHelper(Context context, String name, CursorFactory factory,
+            int version) {
+        super(context, name, factory, version);
+        this.context = context;
+    }
+
+    @Override
+    public Map<String, SQLiteType> getColumns(String table) {
+        return DBUtils.getFields(getReadableDatabase(), table);
+    }
+
+    @Override
+    public List<String> getTables() {
+        return DBUtils.getTables(getReadableDatabase());
+    }
+
+    @Override
+    public List<String> getForeignTables(String table) {
+        return DBUtils.getForeignTables(getReadableDatabase(), table);
+    }
+
+    @Override
+    public int getVersion() {
+        return getReadableDatabase().getVersion();
+    }
+
+    @Override
+    public void setVersion(int version) {
+        getWritableDatabase().setVersion(version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        try {
+            Migrations.migrate(db, context.getAssets(), MIGRATIONS_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.v(TAG, "upgrading database from version " + oldVersion + " to " + newVersion);
-        for (Entry<String, SQLiteTableCreator> entry : createStatements.entrySet()) {
-            if (createdTable.contains(entry.getKey())) {
-                Log.v(TAG, "Table " + entry.getKey() + " already in DB.");
-            } else {
-                Log.v(TAG, "Creating table: " + entry.getKey());
-                SQLiteTableCreator creator = entry.getValue();
-                db.execSQL(DatabaseUtils.getCreateStatement(creator));
-                if (creator.isOneToMany()) {
-                    for (String trigger : creator.getTriggers()) {
-                        db.execSQL(trigger);
-                    }
-                }
-                createdTable.add(entry.getKey());
+        onCreate(db);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        // TODO caching
+    }
+    
+    
+    @Override
+    public Map<String, String> getProjectionMap(String parent, String... foreignTables) {
+        return DBUtils.getProjectionMap(getReadableDatabase(), parent, foreignTables);
+    }
+
+    @Override
+    public List<String> getUniqueConstrains(String table) {
+        if (!constrains.containsKey(table)) {
+            constrains.put(table, DBUtils.getUniqueConstrains(getReadableDatabase(), table));
+        }
+        return constrains.get(table);
+    }
+
+    public String getFirstConstrain(String table, ContentValues values) {
+        List<String> localConstrains = getUniqueConstrains(table);
+        if (localConstrains == null)
+            return null;
+        for (String c : localConstrains) {
+            if (values.containsKey(c)) {
+                return c;
             }
         }
-    }
-
-    public synchronized void createTable(SQLiteTableCreator creator) {
-        if (createdTable.contains(creator.getTableName())) {
-            Log.v(TAG, "Table " + creator.getTableName() + " already in DB.");
-        } else {
-            Log.v(TAG, "Will create table " + creator.getTableName());
-            createStatements.put(creator.getTableName(), creator);
-            getWritableDatabase().needUpgrade(++dbVersion);
-            onUpgrade(getWritableDatabase(), 0, 99);
-        }
-    }
-
-    /**
-     * Method to return the columns and type for a specific table. This only
-     * supports value defined in SQLType
-     * 
-     * @param table , the table name against which we want the columns
-     * @return a map containing all columns and their type
-     */
-    public synchronized Map<String, SQLiteType> getColumnsForTable(final String table) {
-        final Cursor cur = getReadableDatabase().rawQuery(
-                new StringBuilder("PRAGMA table_info('").append(table).append("')").toString(),
-                null);
-        final Map<String, SQLiteType> ret = new HashMap<String, SQLiteType>(cur.getCount());
-        while (cur.moveToNext()) {
-            ret.put(cur.getString(cur.getColumnIndexOrThrow("name")),
-                    SQLiteType.valueOf(cur.getString(cur.getColumnIndexOrThrow("type"))));
-        }
-        cur.close();
-        return ret;
-    }
-
-    /**
-     * Utility method to check if a table has been created in the database or
-     * not.
-     * 
-     * @param tableName , the table to check if created or not
-     * @return true if the table has been created. false otherwise
-     */
-    public synchronized boolean isTableCreated(final String tableName) {
-        return createdTable.contains(tableName);
-    }
-
-    public List<String> getTables() {
         return null;
-    }
-
-    /* package */void executeForeignKeyTrigger() {
-        try {
-
-            String[] cmd = {
-                    "sqlite3", getWritableDatabase().getPath(), ".genfkey --exec"
-            };
-
-            Process process = Runtime.getRuntime().exec(cmd);
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    process.getErrorStream()));
-            BufferedReader reader2 = new BufferedReader(new InputStreamReader(
-                    process.getInputStream()));
-            Log.i(TAG, "c: " + process.waitFor());
-            Log.i(TAG, "e " + reader.readLine());
-            Log.i(TAG, "i " + reader2.readLine());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void execSql(int rawId) {
-        SQLiteFileParser parser = new SQLiteFileParser(context.getResources()
-                .openRawResource(rawId));
-        while (parser.hasNext()) {
-            getWritableDatabase().execSQL(parser.next());
-        }
-        parser.close();
-    }
-
-    public List<String> getForeignTables(String mainTable) {
-        throw new IllegalStateException("not implemented");
     }
 }
