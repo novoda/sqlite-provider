ea99cd0b60813396f9d6bb12eac5bff5c0cb35c7
Carl-Gustaf Harroch <carl@novoda.com>
2011-03-17 15:57:57 +0000

    adding a empty cursor in case we want to return nothing instead of null. Added a loggable provider which just logs and does not have a backed DB

---
+++ src/novoda/lib/sqliteprovider/cursor/EmptyCursor.java
@@ -0,0 +1,55 @@
+
+package novoda.lib.sqliteprovider.cursor;
+
+import android.database.AbstractCursor;
+import android.database.Cursor;
+
+public class EmptyCursor extends AbstractCursor implements Cursor {
+
+    @Override
+    public String[] getColumnNames() {
+        return new String[] {
+            "_id"
+        };
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public double getDouble(int column) {
+        return 0;
+    }
+
+    @Override
+    public float getFloat(int column) {
+        return 0;
+    }
+
+    @Override
+    public int getInt(int column) {
+        return 0;
+    }
+
+    @Override
+    public long getLong(int column) {
+        return 0;
+    }
+
+    @Override
+    public short getShort(int column) {
+        return 0;
+    }
+
+    @Override
+    public String getString(int column) {
+        return null;
+    }
+
+    @Override
+    public boolean isNull(int column) {
+        return true;
+    }
+}
