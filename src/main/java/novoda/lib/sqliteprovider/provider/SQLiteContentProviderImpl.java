3c53cff3adf55d78d68ccc7cf5b3a3b7c4578bf6
Blundell <paul@novoda.com>
2012-11-14 15:11:06 +0000

    Always use bracketed ifs

--- src/main/java/novoda/lib/sqliteprovider/provider/SQLiteContentProviderImpl.java
+++ src/main/java/novoda/lib/sqliteprovider/provider/SQLiteContentProviderImpl.java
@@ -158,19 +158,19 @@ public class SQLiteContentProviderImpl extends SQLiteContentProvider {
         if (Provider.verboseLoggingEnabled()) {
             Provider.v("table: " + builder.getTables());
 
-            if (projection != null)
+            if (projection != null){
                 Provider.v("projection:" + Arrays.toString(projection));
-
-            if (selection != null)
+            }
+            if (selection != null){
                 Provider.v("selection: " + selection + " with arguments "
                         + Arrays.toString(selectionArgs));
-
+            }
             Provider.v("extra args: " + groupBy + " ,having: " + having + " ,sort order: "
                     + sortOrder + " ,limit: " + limit);
 
-            if (autoproj != null)
+            if (autoproj != null){
                 Provider.v("projectionAutomated: " + autoproj);
-
+            }
             Provider.v("==================== end of query =======================");
         }
         
