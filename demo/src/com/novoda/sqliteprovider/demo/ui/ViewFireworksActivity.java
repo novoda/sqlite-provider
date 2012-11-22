a5d1a6146fd5b038e5078f12db2bb8611f5323df
Blundell <paul@novoda.com>
2012-11-22 16:05:24 +0000

    Added a list to eventually show fireworks

--- demo/src/com/novoda/sqliteprovider/demo/ui/ViewFireworksActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/ViewFireworksActivity.java
@@ -1,16 +1,21 @@
 package com.novoda.sqliteprovider.demo.ui;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.novoda.sqliteprovider.demo.R;
 import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
 
 public class ViewFireworksActivity extends NovodaActivity {
 
+	private ListView listview;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_fireworks);
+		
+		listview = (ListView) findViewById(android.R.id.list);
 	}
 	
 }
