527bf6cc4882c1b250706c37bfb8daf9c3bb91fd
Blundell <paul@novoda.com>
2012-11-22 14:58:09 +0000

    Moved DB code to application

--- demo/src/com/novoda/sqliteprovider/demo/NovodaApplication.java
+++ demo/src/com/novoda/sqliteprovider/demo/NovodaApplication.java
@@ -1,7 +1,29 @@
 package com.novoda.sqliteprovider.demo;
 
 import android.app.Application;
+import android.database.Cursor;
+import android.database.sqlite.*;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+
+import novoda.lib.sqliteprovider.sqlite.ExtendedSQLiteOpenHelper;
 
 public class NovodaApplication extends Application {
 
+	private static final String DB_NAME = "fireworks.db";
+	private static final int DB_VERSION = 1;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		ExtendedSQLiteOpenHelper helper = new ExtendedSQLiteOpenHelper(this, DB_NAME, new MyCusorFactory(), DB_VERSION);
+	}
+	
+
+	class MyCusorFactory implements CursorFactory {
+
+		public Cursor newCursor(SQLiteDatabase db, SQLiteCursorDriver masterQuery, String editTable, SQLiteQuery query) {
+			return null;
+		}
+		
+	}
 }
--- demo/src/com/novoda/sqliteprovider/demo/ui/MainActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/MainActivity.java
@@ -1,40 +1,22 @@
 package com.novoda.sqliteprovider.demo.ui;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.*;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.os.Bundle;
 import android.view.View;
 
 import com.novoda.sqliteprovider.demo.R;
 import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
 
-import novoda.lib.sqliteprovider.sqlite.ExtendedSQLiteOpenHelper;
-
 public class MainActivity extends NovodaActivity {
-
-	private static final String DB_NAME = "fireworks.db";
-	private static final int DB_VERSION = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		ExtendedSQLiteOpenHelper helper = new ExtendedSQLiteOpenHelper(this, DB_NAME, new MyCusorFactory(), DB_VERSION);
 	}
 	
 	public void onViewAllFireworksClick(View button){
 		Intent intent = new Intent(this, ViewFireworksActivity.class);
 		startActivity(intent);
-	}
-	
-	class MyCusorFactory implements CursorFactory {
-
-		public Cursor newCursor(SQLiteDatabase db, SQLiteCursorDriver masterQuery, String editTable, SQLiteQuery query) {
-			return null;
-		}
-		
 	}
 }
