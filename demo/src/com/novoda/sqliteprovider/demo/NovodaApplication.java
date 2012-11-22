6a566309a5a38eabcd250bedcf5a4415100f4082
Blundell <paul@novoda.com>
2012-11-22 17:52:25 +0000

    Retrieving from the DB, altho cursor returns 0 atm

--- demo/src/com/novoda/sqliteprovider/demo/NovodaApplication.java
+++ demo/src/com/novoda/sqliteprovider/demo/NovodaApplication.java
@@ -1,9 +1,6 @@
 package com.novoda.sqliteprovider.demo;
 
 import android.app.Application;
-import android.database.Cursor;
-import android.database.sqlite.*;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 
 import com.novoda.sqliteprovider.demo.persistance.DatabaseSetup;
 import com.novoda.sqliteprovider.demo.util.Log;
@@ -29,16 +26,8 @@ public class NovodaApplication extends Application {
 	public ExtendedSQLiteOpenHelper getSqliteHelper() {
 		if(helper == null){
 			Log.i("Helper started for: "+DB_NAME);
-			helper = new ExtendedSQLiteOpenHelper(this, DB_NAME, new MyCusorFactory(), DB_VERSION);
+			helper = new ExtendedSQLiteOpenHelper(this, DB_NAME, null, DB_VERSION);
 		}
 		return helper;
-	}
-
-	class MyCusorFactory implements CursorFactory {
-
-		public Cursor newCursor(SQLiteDatabase db, SQLiteCursorDriver masterQuery, String editTable, SQLiteQuery query) {
-			return null;
-		}
-		
 	}
 }
--- demo/src/com/novoda/sqliteprovider/demo/loader/FireworkLoader.java
+++ demo/src/com/novoda/sqliteprovider/demo/loader/FireworkLoader.java
@@ -1,10 +1,12 @@
 package com.novoda.sqliteprovider.demo.loader;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v4.content.AsyncTaskLoader;
 
 import com.novoda.sqliteprovider.demo.domain.Firework;
+import com.novoda.sqliteprovider.demo.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +28,21 @@ public class FireworkLoader extends AsyncTaskLoader<List<Firework>> {
 
 	@Override
 	public List<Firework> loadInBackground() {
+		Cursor cursor = sqliteHelper.getWritableDatabase().query("fireworks", null, null, new String[]{}, null, null, null);
+//		Cursor cursor = sqliteHelper.getWritableDatabase().rawQuery("SELECT * FROM fireworks", null);
 		ArrayList<Firework> data = new ArrayList<Firework>();
+		if(cursor.moveToFirst()){
+			do {
+				String name = cursor.getString(1);
+				String color = cursor.getString(2);
+				String type = cursor.getString(3);
+				String noise = cursor.getString(4);
+				Firework firework = new Firework(name, color, type, noise);
+				data.add(firework);
+			} while(cursor.moveToNext());
+		} else {
+			Log.e("Database fail");
+		}
 		data.add(new Firework("name", "color", "type", "noise"));
 		return data;
 	}
