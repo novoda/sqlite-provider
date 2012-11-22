8d1d2f1adedec3442bcc28489feeaca8bdaadaf3
Blundell <paul@novoda.com>
2012-11-22 14:12:51 +0000

    Jenkins auto test take 2

--- demo/src/novoda/lib/sqliteprovider/demo/MainActivity.java
+++ demo/src/novoda/lib/sqliteprovider/demo/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// Jenkins test
+		// Jenkins test 2
 	}
 }
