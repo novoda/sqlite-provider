bc29c0e424d5152d11eddf7b551939f865a5dba4
Blundell <paul@novoda.com>
2012-11-22 16:13:01 +0000

    Added loader to start getting fireworks

---
+++ demo/src/com/novoda/sqliteprovider/demo/loader/FireworkLoader.java
@@ -0,0 +1,12 @@
+package com.novoda.sqliteprovider.demo.loader;
+
+import android.content.Context;
+import android.support.v4.content.Loader;
+
+public class FireworkLoader extends Loader<Object> {
+
+	public FireworkLoader(Context context) {
+		super(context);
+	}
+
+}
--- demo/src/com/novoda/sqliteprovider/demo/ui/ViewFireworksActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/ViewFireworksActivity.java
@@ -1,12 +1,15 @@
 package com.novoda.sqliteprovider.demo.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
 import android.widget.ListView;
 
 import com.novoda.sqliteprovider.demo.R;
+import com.novoda.sqliteprovider.demo.loader.FireworkLoader;
 import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
 
-public class ViewFireworksActivity extends NovodaActivity {
+public class ViewFireworksActivity extends NovodaActivity implements LoaderCallbacks<Object>{
 
 	private ListView listview;
 
@@ -16,6 +19,20 @@ public class ViewFireworksActivity extends NovodaActivity {
 		setContentView(R.layout.activity_view_fireworks);
 		
 		listview = (ListView) findViewById(android.R.id.list);
+		
+		getSupportLoaderManager().initLoader(123, null, this);
+	}
+
+	public Loader<Object> onCreateLoader(int id, Bundle args) {
+		return new FireworkLoader(this);
+	}
+
+	public void onLoadFinished(Loader<Object> loader, Object data) {
+		
+	}
+
+	public void onLoaderReset(Loader<Object> loader) {
+		
 	}
 	
 }
