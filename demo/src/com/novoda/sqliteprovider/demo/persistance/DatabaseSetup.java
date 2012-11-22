79585dc49d38b9345917e779393cbed6016e4efa
Blundell <paul@novoda.com>
2012-11-22 15:45:19 +0000

    added logging

--- demo/src/com/novoda/sqliteprovider/demo/persistance/DatabaseSetup.java
+++ demo/src/com/novoda/sqliteprovider/demo/persistance/DatabaseSetup.java
@@ -2,6 +2,8 @@ package com.novoda.sqliteprovider.demo.persistance;
 
 import android.database.sqlite.SQLiteDatabase;
 
+import com.novoda.sqliteprovider.demo.util.Log;
+
 import novoda.lib.sqliteprovider.sqlite.ExtendedSQLiteOpenHelper;
 
 public class DatabaseSetup {
@@ -13,9 +15,15 @@ public class DatabaseSetup {
 	}
 	
 	public void createTables() {
+		Log.i("Creating tables");
+		
 		SQLiteDatabase database = helper.getWritableDatabase();
 		
 		database.execSQL(SqlFireworks.CREATE_TABLE);
+		Log.i("Created Fireworks table");
+
+		
+		Log.i("Created all tables");
 	}
 
 }
