57de318b479a7fd9f0d1ef84dc1e1da77420d7e5
Blundell <paul@novoda.com>
2012-11-22 14:54:34 +0000

    Added button to view fireworks. Added skeleton activiy.

---
+++ demo/src/com/novoda/sqliteprovider/demo/ui/ViewFireworksActivity.java
@@ -0,0 +1,7 @@
+package com.novoda.sqliteprovider.demo.ui;
+
+import android.app.Activity;
+
+public class ViewFireworksActivity extends Activity {
+
+}
---
+++ demo/src/com/novoda/sqliteprovider/demo/ui/base/NovodaActivity.java
@@ -0,0 +1,7 @@
+package com.novoda.sqliteprovider.demo.ui.base;
+
+import android.app.Activity;
+
+public class NovodaActivity extends Activity {
+
+}
