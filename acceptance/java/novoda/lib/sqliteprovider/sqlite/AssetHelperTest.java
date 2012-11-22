d831abef8582d8dd88f40f3337ff0c605fa4b6bb
Blundell <paul@novoda.com>
2012-11-22 10:16:39 +0000

    Moved it which was the acceptance tests into an acceptance folder. Created a demo folder to create a demo into. Modified the poms to make it a module with submodules.

---
+++ acceptance/java/novoda/lib/sqliteprovider/sqlite/AssetHelperTest.java
@@ -0,0 +1,17 @@
+
+package novoda.lib.sqliteprovider.sqlite;
+
+import android.test.InstrumentationTestCase;
+
+public class AssetHelperTest extends InstrumentationTestCase {
+
+    public AssetHelperTest() {
+        super();
+    }
+
+    public void testSomething() throws Exception {
+        String[] strings = getInstrumentation().getContext().getAssets().list("sql");
+        
+        assertEquals(strings[0], "create.sql");
+    }
+}
