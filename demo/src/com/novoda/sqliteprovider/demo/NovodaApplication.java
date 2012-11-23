45e6481905653878df5706aba490645886770765
Blundell <paul@novoda.com>
2012-11-23 15:55:47 +0000

    Removed now redundant files and code for SQL creation

--- demo/src/com/novoda/sqliteprovider/demo/NovodaApplication.java
+++ demo/src/com/novoda/sqliteprovider/demo/NovodaApplication.java
@@ -1,38 +1,17 @@
 package com.novoda.sqliteprovider.demo;
 
 import android.app.Application;
-import android.database.sqlite.SQLiteOpenHelper;
 
 import com.novoda.sqliteprovider.demo.persistance.DatabaseReader;
 
 public class NovodaApplication extends Application {
 
-	private DatabaseReader databaseHelper;
-	private SQLiteOpenHelper sqLiteOpenHelper;
+	private DatabaseReader databaseReader;
 	
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		String databaseName = getPackageName()+".db";
-		deleteDatabase(databaseName);
-		
-//		sqLiteOpenHelper = new ExtendedSQLiteOpenHelper(this, databaseName, null, 1);
-//		try {
-//			sqLiteOpenHelper = new ExtendedSQLiteOpenHelper(this);
-//		} catch (IOException e) {
-//			Log.e("Database error", e);
-//		}
-//		SQLiteDatabase writableDatabase = sqLiteOpenHelper.getWritableDatabase();
-		
-//			DatabaseSetup databaseSetup = new DatabaseSetup(sqLiteOpenHelper);
-//			databaseSetup.createTables();
-//			databaseSetup.addStaticData();
-	}
-
 	public DatabaseReader getDatabaseReader() {
-		if(databaseHelper == null){
-			databaseHelper = new DatabaseReader(getContentResolver());
+		if(databaseReader == null){
+			databaseReader = new DatabaseReader(getContentResolver());
 		}
-		return databaseHelper;
+		return databaseReader;
 	}
 }
