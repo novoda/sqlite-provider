d3536b052aa27d7fbee3c9f5d4586aca8fadcdb7
Blundell <paul@novoda.com>
2012-11-23 16:12:26 +0000

    Added skeleton for adding a firework activity

---
+++ demo/src/com/novoda/sqliteprovider/demo/ui/AddFireworkActivity.java
@@ -0,0 +1,16 @@
+package com.novoda.sqliteprovider.demo.ui;
+
+import android.os.Bundle;
+
+import com.novoda.sqliteprovider.demo.R;
+import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
+
+public class AddFireworkActivity extends NovodaActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_firework);
+	}
+	
+}
