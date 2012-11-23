ab1252f60b787f56710ac97518a02ea9bd77823e
Blundell <paul@novoda.com>
2012-11-23 16:36:08 +0000

    Added references to buttons

--- demo/src/com/novoda/sqliteprovider/demo/ui/AddFireworkActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/AddFireworkActivity.java
@@ -2,16 +2,27 @@ package com.novoda.sqliteprovider.demo.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.novoda.sqliteprovider.demo.R;
 import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
 
 public class AddFireworkActivity extends NovodaActivity {
 
+	private EditText nameEditText;
+	private EditText colorEditText;
+	private EditText noiseEditText;
+	private EditText typeEditText;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_firework);
+		
+		nameEditText = (EditText) findViewById(R.id.add_firework_input_name);
+		colorEditText = (EditText) findViewById(R.id.add_firework_input_color);
+		noiseEditText = (EditText) findViewById(R.id.add_firework_input_noise);
+		typeEditText = (EditText) findViewById(R.id.add_firework_input_type);
 	}
 	
 	public void onAddFireworkClick(View button){
