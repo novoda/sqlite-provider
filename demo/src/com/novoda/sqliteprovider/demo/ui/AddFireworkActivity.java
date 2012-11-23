47d1d43762b318f46e67d66af129acce18f101b9
Blundell <paul@novoda.com>
2012-11-23 16:35:01 +0000

    Added button and click listener for adding firework

--- demo/src/com/novoda/sqliteprovider/demo/ui/AddFireworkActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/AddFireworkActivity.java
@@ -1,6 +1,7 @@
 package com.novoda.sqliteprovider.demo.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.novoda.sqliteprovider.demo.R;
 import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
@@ -11,6 +12,10 @@ public class AddFireworkActivity extends NovodaActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_firework);
+	}
+	
+	public void onAddFireworkClick(View button){
+		
 	}
 	
 }
