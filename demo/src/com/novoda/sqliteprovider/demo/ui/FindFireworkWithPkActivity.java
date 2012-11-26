60af3e93171f202410a8a77494dcd86a01ce7fa7
Blundell <paul@novoda.com>
2012-11-26 11:28:07 +0000

    Added find firework with PK activity skeleton

---
+++ demo/src/com/novoda/sqliteprovider/demo/ui/FindFireworkWithPkActivity.java
@@ -0,0 +1,15 @@
+package com.novoda.sqliteprovider.demo.ui;
+
+import android.os.Bundle;
+
+import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
+
+public class FindFireworkWithPkActivity extends NovodaActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+}
