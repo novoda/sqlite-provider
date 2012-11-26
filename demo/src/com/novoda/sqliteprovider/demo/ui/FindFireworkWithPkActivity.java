df20d616820cc78c7624b64de8c824220054473d
Blundell <paul@novoda.com>
2012-11-26 11:28:42 +0000

    Added find firework with PK activity skeleton

--- demo/src/com/novoda/sqliteprovider/demo/ui/FindFireworkWithPkActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/FindFireworkWithPkActivity.java
@@ -2,6 +2,7 @@ package com.novoda.sqliteprovider.demo.ui;
 
 import android.os.Bundle;
 
+import com.novoda.sqliteprovider.demo.R;
 import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
 
 public class FindFireworkWithPkActivity extends NovodaActivity {
@@ -9,7 +10,7 @@ public class FindFireworkWithPkActivity extends NovodaActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.activity_find_firework_with_pk);
 	}
 	
 }
