7c2f1896c0fc0c5b99b3ab6e3ac6fa4606a8f00a
Blundell <paul@novoda.com>
2012-11-26 11:45:46 +0000

    Can now click through list to see firework data

--- demo/src/com/novoda/sqliteprovider/demo/ui/ViewFireworksActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/ViewFireworksActivity.java
@@ -1,11 +1,13 @@
 package com.novoda.sqliteprovider.demo.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.*;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.novoda.sqliteprovider.demo.R;
 import com.novoda.sqliteprovider.demo.domain.Firework;
@@ -21,12 +23,14 @@ public class ViewFireworksActivity extends NovodaActivity implements LoaderCallb
 	private ListView listview;
 	private final Handler uiHook = new Handler();
 	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_fireworks);
 		
 		listview = (ListView) findViewById(android.R.id.list);
+		listview.setOnItemClickListener(onFireworkListItemClick );
 		
 		getSupportLoaderManager().initLoader(123, null, this);
 	}
@@ -53,6 +57,19 @@ public class ViewFireworksActivity extends NovodaActivity implements LoaderCallb
 		});
 	}
 
+	private final OnItemClickListener onFireworkListItemClick = new OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+			viewFirework((Firework)listview.getItemAtPosition(position));
+		}
+	};
+	
+	private void viewFirework(Firework firework) {
+		Intent intent = new Intent(this, FireworkActivity.class);
+		intent.putExtra(FireworkActivity.EXTRA_FIREWORK, firework);
+		startActivity(intent);
+	}
+	
 	@Override
 	public void onLoaderReset(Loader<List<Firework>> loader) {
 		
