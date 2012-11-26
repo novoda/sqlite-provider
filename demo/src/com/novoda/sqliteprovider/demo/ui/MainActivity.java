21a5f2a2c275027be4f03846441b2f6794961764
Blundell <paul@novoda.com>
2012-11-26 11:07:57 +0000

    Added annotation to highlight entry points. Idea is to use this to turn of eclipse warning for unused input variable, as the input var is needed for android reflection

--- demo/src/com/novoda/sqliteprovider/demo/ui/MainActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import com.novoda.sqliteprovider.demo.R;
 import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
+import com.novoda.sqliteprovider.demo.ui.util.FromXML;
 
 public class MainActivity extends NovodaActivity {
 
@@ -15,36 +16,44 @@ public class MainActivity extends NovodaActivity {
 		setContentView(R.layout.activity_main);
 	}
 
+	@FromXML
 	public void onViewAllFireworksClick(View button) {
 		Intent intent = new Intent(this, ViewFireworksActivity.class);
 		startActivity(intent);
 	}
 
+	@FromXML
 	public void onAddFireworkClick(View button) {
 		Intent intent = new Intent(this, AddFireworkActivity.class);
 		startActivity(intent);
 	}
 
+	@FromXML
 	public void onFindFireworkWithPrimaryKeyClick(View button) {
 
 	}
 
+	@FromXML
 	public void onFindAllFireworksFromOneShopClick(View button) {
 
 	}
 
+	@FromXML
 	public void onFindAllRedFireworksClick(View button) {
 
 	}
 
+	@FromXML
 	public void onFindAllWizzFireworksClick(View button) {
 
 	}
 
+	@FromXML
 	public void onFindThreeFireworkShopsClick(View button) {
 
 	}
 
+	@FromXML
 	public void onFindUniquelyNoisyFireworksClick(View button) {
 
 	}
