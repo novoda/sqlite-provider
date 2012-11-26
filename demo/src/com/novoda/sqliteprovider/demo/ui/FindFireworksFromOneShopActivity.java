6dc33b3909d0c3417245421c0224bb621f143b72
Blundell <paul@novoda.com>
2012-11-26 13:01:46 +0000

    Added find from shop layout views

--- demo/src/com/novoda/sqliteprovider/demo/ui/FindFireworksFromOneShopActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/FindFireworksFromOneShopActivity.java
@@ -1,9 +1,11 @@
 package com.novoda.sqliteprovider.demo.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.novoda.sqliteprovider.demo.R;
 import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
+import com.novoda.sqliteprovider.demo.ui.util.FromXML;
 
 public class FindFireworksFromOneShopActivity extends NovodaActivity {
 
@@ -11,6 +13,11 @@ public class FindFireworksFromOneShopActivity extends NovodaActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_find_firework_from_one_shop);
+	}
+	
+	@FromXML
+	public void onFindShopFireworks(View button){
+		
 	}
 	
 }
