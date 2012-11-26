c3258739a5c45aab13fe0ee8a65e45eaae7759ab
Blundell <paul@novoda.com>
2012-11-26 11:30:20 +0000

    Added skeleton to view a firework, save duplicating everywhere needed

---
+++ demo/src/com/novoda/sqliteprovider/demo/ui/FireworkActivity.java
@@ -0,0 +1,16 @@
+package com.novoda.sqliteprovider.demo.ui;
+
+import android.os.Bundle;
+
+import com.novoda.sqliteprovider.demo.R;
+import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
+
+public class FireworkActivity extends NovodaActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_firework);
+	}
+	
+}
