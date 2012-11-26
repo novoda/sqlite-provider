bb9c72628d4661b6de677ba6fa4d949221c84a9c
Blundell <paul@novoda.com>
2012-11-26 16:39:47 +0000

    Changed SQL to convention for parent child tables. Removed keyword column title type

--- demo/src/com/novoda/sqliteprovider/demo/persistance/DatabaseConstants.java
+++ demo/src/com/novoda/sqliteprovider/demo/persistance/DatabaseConstants.java
@@ -7,8 +7,8 @@ public class DatabaseConstants {
 		protected static final String COL_NAME = "name";
 		protected static final String COL_COLOR = "color";
 		protected static final String COL_NOISE = "noise";
-		protected static final String COL_TYPE = "type";
-		protected static final String COL_SHOP = "shop";
+		protected static final String COL_TYPE = "ftype";
+		protected static final String COL_SHOP = "shops_id";
 		protected static final int COL_IDX_NAME = 1;
 		protected static final int COL_IDX_COLOR = 2;
 		protected static final int COL_IDX_TYPE = 3;
