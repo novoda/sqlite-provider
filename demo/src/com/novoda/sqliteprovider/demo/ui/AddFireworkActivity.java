68cc60b8e65d14b3ec57c32a611d44479d8f69fe
Blundell <paul@novoda.com>
2012-11-30 15:39:43 +0000

    Moved adding fireworks into a fragment

--- demo/src/com/novoda/sqliteprovider/demo/ui/AddFireworkActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/AddFireworkActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
 import android.text.TextUtils;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.novoda.sqliteprovider.demo.R;
@@ -14,17 +12,11 @@ import com.novoda.sqliteprovider.demo.domain.UseCaseFactory.UseCase;
 import com.novoda.sqliteprovider.demo.loader.FireworkSaver;
 import com.novoda.sqliteprovider.demo.persistance.DatabaseConstants.RawSql;
 import com.novoda.sqliteprovider.demo.ui.base.NovodaActivity;
+import com.novoda.sqliteprovider.demo.ui.fragment.AddFireworkFragment.AddFireworkListener;
 import com.novoda.sqliteprovider.demo.ui.fragment.UriSqlFragment;
-import com.novoda.sqliteprovider.demo.ui.input.AddFirework;
-import com.novoda.sqliteprovider.demo.ui.util.FromXML;
 
-public class AddFireworkActivity extends NovodaActivity implements LoaderCallbacks<Firework>, AddFirework {
+public class AddFireworkActivity extends NovodaActivity implements AddFireworkListener, LoaderCallbacks<Firework> {
 
-	private EditText nameEditText;
-	private EditText colorEditText;
-	private EditText noiseEditText;
-	private EditText typeEditText;
-	private EditText priceEditText;
 	private UriSqlFragment uriSqlFragment;
 	private Firework firework;
 
@@ -33,46 +25,21 @@ public class AddFireworkActivity extends NovodaActivity implements LoaderCallbac
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_firework);
 		
-		nameEditText = (EditText) findViewById(R.id.add_firework_input_name);
-		colorEditText = (EditText) findViewById(R.id.add_firework_input_color);
-		noiseEditText = (EditText) findViewById(R.id.add_firework_input_noise);
-		typeEditText = (EditText) findViewById(R.id.add_firework_input_type);
-		priceEditText = (EditText) findViewById(R.id.add_firework_input_price);
-		
 		uriSqlFragment = (UriSqlFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_uri_sql);
 		uriSqlFragment.setInfo(UseCase.ADD);
 	}
 	
 	@Override
-	@FromXML
-	public void onAddFireworkClick(View button){
-		if(checkForInput(nameEditText) || checkForInput(colorEditText) ||
-				checkForInput(noiseEditText) || checkForInput(typeEditText) || checkForInput(priceEditText)) {
-			Toast.makeText(this, "Fill in the firework", Toast.LENGTH_SHORT).show();
-			return;
-		}
-
-		String name = "";
-		String color = "";
-		String noise = "";
-		String type = "";
-		double price = 0;
-		
-		name = nameEditText.getText().toString();
-		color = colorEditText.getText().toString();
-		noise = noiseEditText.getText().toString();
-		type = typeEditText.getText().toString();
-		price = Double.parseDouble(priceEditText.getText().toString());
-		
-		firework = new Firework(name, color, type, noise, price);
-		
+	public void onEmptyInput() {
+		Toast.makeText(this, "Fill in the firework", Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public void onAddClick(Firework firework) {
+		this.firework = firework;
 		getSupportLoaderManager().initLoader(FireworkSaver.LOADER_ID, null, this);
 	}
-
-	private boolean checkForInput(EditText editText) {
-		return TextUtils.isEmpty(editText.getText());
-	}
-
+	
 	@Override
 	public Loader<Firework> onCreateLoader(int id, Bundle args) {
 		return new FireworkSaver(this, getApp().getFireworkWriter(), firework);
@@ -82,12 +49,6 @@ public class AddFireworkActivity extends NovodaActivity implements LoaderCallbac
 	public void onLoadFinished(Loader<Firework> loader, Firework data) {
 		Toast.makeText(this, "Firework that goes "+ data.getNoise() +" added.", Toast.LENGTH_SHORT).show();
 		
-		nameEditText.setText("");
-		colorEditText.setText("");
-		noiseEditText.setText("");
-		typeEditText.setText("");
-		priceEditText.setText("");
-	
 		uriSqlFragment.updateSql(createSQL(data));
 	}
 
