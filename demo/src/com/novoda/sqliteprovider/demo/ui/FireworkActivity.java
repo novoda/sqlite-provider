c29a7f8fd9022fa3aff0f483c59ed6fce1771c53
Blundell <paul@novoda.com>
2012-11-30 14:40:40 +0000

    protected again now moved intent back to activity

--- demo/src/com/novoda/sqliteprovider/demo/ui/FireworkActivity.java
+++ demo/src/com/novoda/sqliteprovider/demo/ui/FireworkActivity.java
@@ -10,7 +10,7 @@ import com.novoda.sqliteprovider.demo.util.Log;
 
 public class FireworkActivity extends NovodaActivity {
 
-	public static final String EXTRA_FIREWORK = "com.novoda.sqliteprovider.demo.EXTRA_FIREWORK";
+	protected static final String EXTRA_FIREWORK = "com.novoda.sqliteprovider.demo.EXTRA_FIREWORK";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
