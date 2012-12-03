3ee1c5a7a53fbeaed61deb442970200e352b2d29
Blundell <blundell.app.dev@gmail.com>
2012-12-03 10:24:58 +0000

    Added before annotation for setup, looks like this test class isnt being run. Because it ends in Test & not Tests?

--- core/src/test/java/novoda/lib/sqliteprovider/util/UriToSqlAttributesInspectorTest.java
+++ core/src/test/java/novoda/lib/sqliteprovider/util/UriToSqlAttributesInspectorTest.java
@@ -1,36 +1,35 @@
 package novoda.lib.sqliteprovider.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
 
 import android.net.Uri;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 @RunWith(RoboRunner.class)
 public class UriToSqlAttributesInspectorTest {
-	
+
 	private UriInspector uriInspector = new UriInspector();;
 	private UriToSqlAttributes attrs = null;
 
+	@Before
 	private void setup() {
 		uriInspector = new UriInspector();
 	}
-	
-    @Test
-	public void testUriInspectorCreationBehaviour(){
+
+	@Test
+	public void testUriInspectorCreationBehaviour() {
 		attrs = uriInspector.parse(Uri.parse("content://test.com/item/1"));
 		assertEquals("content://test.com/item/1", attrs.getUri().toString());
 	}
-    
-    @Test
-	public void testHasWhereClauseInQuery(){
-    	attrs = uriInspector.parse(Uri.parse("content://test.com/item/1"));
+
+	@Test
+	public void testHasWhereClauseInQuery() {
+		attrs = uriInspector.parse(Uri.parse("content://test.com/item/1"));
 		assertFalse(attrs.hasWhereClauseInQuery());
-    	attrs = uriInspector.parse(Uri.parse("content://test.com/tableName?groupBy=col&having=value"));
-    	assertTrue(attrs.hasWhereClauseInQuery());
-    }
+		attrs = uriInspector.parse(Uri.parse("content://test.com/tableName?groupBy=col&having=value"));
+		assertTrue(attrs.hasWhereClauseInQuery());
+	}
 }
