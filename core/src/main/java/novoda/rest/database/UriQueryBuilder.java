1a952ecaf8ecd96644b5469b0bb5950a2989f091
Blundell <blundell.app.dev@gmail.com>
2012-12-03 10:21:35 +0000

    Redundant else removed

--- core/src/main/java/novoda/rest/database/UriQueryBuilder.java
+++ core/src/main/java/novoda/rest/database/UriQueryBuilder.java
@@ -1,4 +1,3 @@
-
 package novoda.rest.database;
 
 import android.database.sqlite.SQLiteQueryBuilder;
@@ -8,68 +7,67 @@ import java.util.*;
 
 public class UriQueryBuilder extends SQLiteQueryBuilder {
 
-    private List<String> pathSegments;
+	private List<String> pathSegments;
 
-    private Uri uri;
+	private Uri uri;
 
-    public UriQueryBuilder(final Uri uri) {
-        this.setUri(uri);
-    }
+	public UriQueryBuilder(final Uri uri) {
+		this.setUri(uri);
+	}
 
-    public static SQLiteQueryBuilder fromUri(final Uri uri) {
-        return new UriQueryBuilder(uri);
-    }
+	public static SQLiteQueryBuilder fromUri(final Uri uri) {
+		return new UriQueryBuilder(uri);
+	}
 
-    public String getTable() {
-        if (isDirectory()){
-            return pathSegments.get(pathSegments.size() - 1);
-        } else {
-            return pathSegments.get(pathSegments.size() - 2);
-        }
-    }
+	public String getTable() {
+		if (isDirectory()) {
+			return pathSegments.get(pathSegments.size() - 1);
+		}
+		return pathSegments.get(pathSegments.size() - 2);
+	}
 
-    public boolean isDirectory() {
-        return (pathSegments.size() % 2 == 1);
-    }
+	public boolean isDirectory() {
+		return (pathSegments.size() % 2 == 1);
+	}
 
-    public boolean isOneToMany() {
-        return (pathSegments.size() > 2);
-    }
+	public boolean isOneToMany() {
+		return (pathSegments.size() > 2);
+	}
 
-    public void setUri(Uri uri) {
-        this.uri = uri;
-        init();
-    }
+	public void setUri(Uri uri) {
+		this.uri = uri;
+		init();
+	}
 
-    private void init() {
-        pathSegments = new ArrayList<String>(Arrays.asList(uri.getPath().split("/")));
-        pathSegments.remove(0);
-        setTables(getTable());
-    }
+	private void init() {
+		pathSegments = new ArrayList<String>(Arrays.asList(uri.getPath().split("/")));
+		pathSegments.remove(0);
+		setTables(getTable());
+	}
 
-    public String getWhere() {
-        StringBuffer buf = new StringBuffer();
-        if (!isDirectory()) {
-            buf.append(getPKColumn()).append("=").append(pathSegments.get(pathSegments.size() - 1));
-        }
-        if (isOneToMany()) {
-            if (!isDirectory()) {
-                buf.append(" AND ");
-            }
-            buf.append(getFKColumn()).append("=").append(pathSegments.get(pathSegments.size() - 3));
-        }
-        return buf.toString();
-    }
+	public String getWhere() {
+		StringBuffer buf = new StringBuffer();
+		if (!isDirectory()) {
+			buf.append(getPKColumn()).append("=").append(pathSegments.get(pathSegments.size() - 1));
+		}
+		if (isOneToMany()) {
+			if (!isDirectory()) {
+				buf.append(" AND ");
+			}
+			buf.append(getFKColumn()).append("=").append(pathSegments.get(pathSegments.size() - 3));
+		}
+		return buf.toString();
+	}
 
-    private String getPKColumn() {
-        return "_id";
-    }
+	private String getPKColumn() {
+		return "_id";
+	}
 
-    private String getFKColumn() {
-        return pathSegments.get(pathSegments.size() - 4) + "_id";
-    }
+	private String getFKColumn() {
+		return pathSegments.get(pathSegments.size() - 4) + "_id";
+	}
 
-    public Uri getUri() {
-        return uri;
-    }
+	public Uri getUri() {
+		return uri;
+	}
 }
