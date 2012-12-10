09e90966280ab7db147b73b42c6799c933efe87f
Blundell <paul@novoda.com>
2012-12-10 12:58:12 +0000

    Removed trailing white space, you bad boy

--- demo/src/com/novoda/sqliteprovider/demo/NovodaApplication.java
+++ demo/src/com/novoda/sqliteprovider/demo/NovodaApplication.java
@@ -10,28 +10,28 @@ public class NovodaApplication extends Application {
 	private DatabaseWriter databaseWriter;
 	private FireworkReader fireworkReader;
 	private FireworkWriter fireworkWriter;
-	
+
 	private DatabaseReader getDatabaseReader() {
 		if(databaseReader == null){
 			databaseReader = new DatabaseReader(getContentResolver());
 		}
 		return databaseReader;
 	}
-	
+
 	private DatabaseWriter getDatabaseWriter() {
 		if(databaseWriter == null){
 			databaseWriter = new DatabaseWriter(getContentResolver());
 		}
 		return databaseWriter;
 	}
-	
+
 	public final FireworkReader getFireworkReader() {
 		if(fireworkReader == null){
 			fireworkReader = new FireworkReader(getDatabaseReader());
 		}
 		return fireworkReader;
 	}
-	
+
 	public final FireworkWriter getFireworkWriter() {
 		if(fireworkWriter == null){
 			fireworkWriter = new FireworkWriter(getDatabaseWriter());
