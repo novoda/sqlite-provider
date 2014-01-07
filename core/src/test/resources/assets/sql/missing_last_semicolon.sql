7a90993ba3f33aac003461e3eaa2790ecb5da0ae
Volker Leck <volker.leck@gmail.com>
2014-01-07 12:11:29 +0100

    add comment to sql file to explain its usage

--- core/src/test/resources/assets/sql/missing_last_semicolon.sql
+++ core/src/test/resources/assets/sql/missing_last_semicolon.sql
@@ -1,3 +1,4 @@
+-- We ommit the trailing semicolon of the last statement to test whether the parser detects that.
 CREATE TABLE 'testTable'
         _id INTEGER PRIMARY KEY AUTOINCREMENT;
 CREATE TABLE 'second'
