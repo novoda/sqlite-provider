bebe0a3a48ee84110facf6699a6924cb5c05b0cf
Blundell <blundell.app.dev@gmail.com>
2014-01-16 18:40:51 +0000

    Removed all unused classes - made simple demo

--- demo-simple/src/com/novoda/sqliteprovider/demo/simple/provider/FireworkProvider.java
+++ demo-simple/src/com/novoda/sqliteprovider/demo/simple/provider/FireworkProvider.java
@@ -1,9 +1,12 @@
 package com.novoda.sqliteprovider.demo.simple.provider;
 
+import android.net.Uri;
+
 import novoda.lib.sqliteprovider.provider.SQLiteContentProviderImpl;
 
 public class FireworkProvider extends SQLiteContentProviderImpl {
 
-    public static final String AUTHORITY = "content://com.novoda.demo/";
-
+    public static final String AUTHORITY = "content://com.novoda.demo.simple/";
+    private static final String TABLE_SHOP = "shop";
+    public static final Uri SELECT_SHOPS = Uri.parse(AUTHORITY).buildUpon().appendPath(TABLE_SHOP).build();
 }
---
+++ demo-simple/src/com/novoda/sqliteprovider/demo/simple/ui/MainFragment.java
@@ -0,0 +1,62 @@
+package com.novoda.sqliteprovider.demo.simple.ui;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.novoda.sqliteprovider.demo.simple.R;
+import com.novoda.sqliteprovider.demo.simple.provider.FireworkProvider;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().getSupportLoaderManager()
+                .initLoader(R.id.loader_shop, null, new LoaderManager.LoaderCallbacks<Cursor>() {
+
+                    @Override
+                    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+                        return new ShopCursorLoader(getActivity());
+                    }
+
+                    @Override
+                    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+                        if (!cursor.moveToFirst()) {
+                            Log.d("demo", "Nothing in DB, returning early");
+                        }
+
+                        do {
+                            Log.d("demo", "Found shop:" + cursor.getString(cursor.getColumnIndex("name")));
+                            Log.d("demo", "Found shop postcode:" + cursor.getString(cursor.getColumnIndex("postcode")));
+                        } while (cursor.moveToNext());
+
+                    }
+
+                    @Override
+                    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+
+                    }
+                });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    private static class ShopCursorLoader extends CursorLoader {
+
+        public ShopCursorLoader(Context context) {
+            super(context, FireworkProvider.SELECT_SHOPS, null, null, null, null);
+        }
+    }
+}
