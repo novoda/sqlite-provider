f053cc77ae8eaa1fbe14fad0ee35d11534e27026
Conor O'Donnell <mrconor@gmail.com>
2015-05-05 19:33:37 +0100

    Re-added and deprecated class to prevent incompatibility problems

---
+++ core/src/main/java/novoda/lib/sqliteprovider/util/DatabaseUtils.java
@@ -0,0 +1,35 @@
+package novoda.lib.sqliteprovider.util;
+
+import android.content.ContentValues;
+
+import novoda.rest.database.SQLiteTableCreator;
+
+public class DatabaseUtils extends android.database.DatabaseUtils {
+
+
+    /**
+     * @deprecated
+     * Use {@link DBUtils#contentValuestoTableCreate(ContentValues, String)} instead
+     *
+     */
+    @Deprecated
+    public static String contentValuestoTableCreate(ContentValues values, String table) {
+        return DBUtils.contentValuestoTableCreate(values, table);
+    }
+
+    /**
+     * @deprecated Use {@link DBUtils#getCreateStatement(SQLiteTableCreator)} instead
+     */
+    @Deprecated
+    public static String getCreateStatement(SQLiteTableCreator creator) {
+        return DBUtils.getCreateStatement(creator);
+    }
+
+    /**
+     * @deprecated Use {@link DBUtils#getSQLiteVersion()} instead
+     */
+    @Deprecated
+    public static String getSQLiteVersion() {
+        return DBUtils.getSQLiteVersion();
+    }
+}
