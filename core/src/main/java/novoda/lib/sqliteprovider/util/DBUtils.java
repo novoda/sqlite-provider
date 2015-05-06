f29a60a7b41e04ff875bb4b1181ff9f4afe6325a
Conor O'Donnell <mrconor@gmail.com>
2015-05-06 21:08:49 +0100

    Move DBUtils to using DatabaseAnalyzer and StatementGenerator

--- core/src/main/java/novoda/lib/sqliteprovider/util/DBUtils.java
+++ core/src/main/java/novoda/lib/sqliteprovider/util/DBUtils.java
@@ -6,48 +6,28 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import novoda.lib.sqliteprovider.sqlite.IDatabaseMetaInfo.SQLiteType;
+import novoda.lib.sqliteprovider.util.analyzer.DatabaseAnalyzer;
+import novoda.lib.sqliteprovider.util.analyzer.StatementGenerator;
 import novoda.rest.database.SQLiteTableCreator;
 
 public final class DBUtils {
 
-    private static final String SELECT_TABLES_NAME = "SELECT name FROM sqlite_master WHERE type='table';";
     private static final String PRAGMA_TABLE = "PRAGMA table_info(\"%1$s\");";
     private static final String PRAGMA_INDEX_LIST = "PRAGMA index_list('%1$s');";
     private static final String PRAGMA_INDEX_INFO = "PRAGMA index_info('%1$s');";
-
-    private static List<String> defaultTables = Arrays.asList("android_metadata");
 
     private DBUtils() {
         // Util class
     }
 
     public static List<String> getForeignTables(SQLiteDatabase db, String table) {
-        final Cursor cur = db.rawQuery(String.format(PRAGMA_TABLE, table), null);
-        List<String> tables = getTables(db);
-        List<String> foreignTables = new ArrayList<String>(5);
-        String name;
-        String tableName;
-        while (cur.moveToNext()) {
-            name = cur.getString(cur.getColumnIndexOrThrow("name"));
-            if (name.endsWith("_id")) {
-                tableName = name.substring(0, name.lastIndexOf('_'));
-                if (tables.contains(tableName + "s")) {
-                    foreignTables.add(tableName + "s");
-                } else if (tables.contains(tableName)) {
-                    foreignTables.add(tableName);
-                }
-            }
-        }
-        cur.close();
-        return Collections.unmodifiableList(foreignTables);
+        return new DatabaseAnalyzer(db).getForeignTables(table);
     }
 
     /**
@@ -55,34 +35,11 @@ public final class DBUtils {
      * @return a list of tables
      */
     public static List<String> getTables(SQLiteDatabase db) {
-        final Cursor cur = db.rawQuery(SELECT_TABLES_NAME, null);
-        List<String> createdTable = new ArrayList<String>(cur.getCount());
-        String tableName;
-        while (cur.moveToNext()) {
-            tableName = cur.getString(0);
-            if (!defaultTables.contains(tableName)) {
-                createdTable.add(tableName);
-            }
-        }
-        cur.close();
-        return Collections.unmodifiableList(createdTable);
+        return new DatabaseAnalyzer(db).getTableNames();
     }
 
     public static Map<String, String> getProjectionMap(SQLiteDatabase db, String parent, String... foreignTables) {
-        Map<String, String> projection = new HashMap<String, String>();
-        projection.put("_id", parent + "._id AS _id");
-        for (Entry<String, SQLiteType> entry : getFields(db, parent).entrySet()) {
-            projection.put(parent + "_" + entry.getKey(), parent + "." + entry.getKey() + " AS "
-                    + parent + "_" + entry.getKey());
-        }
-
-        for (String ft : foreignTables) {
-            for (Entry<String, SQLiteType> entry : getFields(db, ft).entrySet()) {
-                projection.put(ft + "_" + entry.getKey(), ft + "." + entry.getKey() + " AS " + ft
-                        + "_" + entry.getKey());
-            }
-        }
-        return Collections.unmodifiableMap(projection);
+        return new DatabaseAnalyzer(db).getProjectionMap(parent, foreignTables);
     }
 
     public static Map<String, SQLiteType> getFields(SQLiteDatabase db, String table) {
@@ -105,14 +62,8 @@ public final class DBUtils {
      * @return the SQLite version
      */
     public static String getSQLiteVersion() {
-        final Cursor cursor = SQLiteDatabase.openOrCreateDatabase(":memory:", null).rawQuery(
-                "select sqlite_version() AS sqlite_version", null);
-        StringBuilder sqliteVersion = new StringBuilder();
-        while (cursor.moveToNext()) {
-            sqliteVersion.append(cursor.getString(0));
-        }
-        cursor.close();
-        return sqliteVersion.toString();
+        final SQLiteDatabase sqLiteDatabase = SQLiteDatabase.openOrCreateDatabase(":memory:", null);
+        return new DatabaseAnalyzer(sqLiteDatabase).getSQLiteVersion();
     }
 
     /**
@@ -138,80 +89,14 @@ public final class DBUtils {
     }
 
     public static List<Constraint> getUniqueConstraints(SQLiteDatabase db, String table) {
-        List<Constraint> constraints = new ArrayList<Constraint>();
-        final Cursor indexCursor = db.rawQuery(String.format(PRAGMA_INDEX_LIST, table), null);
-        while (indexCursor.moveToNext()) {
-            int isUnique = indexCursor.getInt(2);
-            if (isUnique == 1) {
-                String indexName = indexCursor.getString(1);
-                final Cursor columnCursor = db.rawQuery(String.format(PRAGMA_INDEX_INFO, indexName), null);
-                List<String> columns = new ArrayList<>(columnCursor.getCount());
-                while (columnCursor.moveToNext()) {
-                    String columnName = columnCursor.getString(2);
-                    columns.add(columnName);
-                }
-                columnCursor.close();
-                constraints.add(new Constraint(columns));
-            }
-        }
-        indexCursor.close();
-        return constraints;
+        return new DatabaseAnalyzer(db).getUniqueConstraints(table);
     }
 
     public static String contentValuestoTableCreate(ContentValues values, String table) {
-        StringBuffer buf = new StringBuffer("CREATE TABLE ").append(table).append(" (");
-        for (Entry<String, Object> entry : values.valueSet()) {
-            buf.append(entry.getKey()).append(" TEXT").append(", ");
-        }
-        buf.delete(buf.length() - 2, buf.length());
-        buf.append(");");
-        return buf.toString();
+        return new StatementGenerator().contentValuestoTableCreate(values, table);
     }
 
     public static String getCreateStatement(SQLiteTableCreator creator) {
-
-        String primaryKeyColumnName = creator.getPrimaryKey();
-        novoda.rest.database.SQLiteType primaryKeyColumnType;
-        boolean shouldAutoincrement;
-        if (primaryKeyColumnName == null) {
-            primaryKeyColumnName = "_id";
-            primaryKeyColumnType = novoda.rest.database.SQLiteType.INTEGER;
-            shouldAutoincrement = true;
-        } else {
-            primaryKeyColumnType = creator.getType(primaryKeyColumnName);
-            shouldAutoincrement = creator.shouldPKAutoIncrement();
-        }
-
-
-        StringBuilder sql = new StringBuilder().append("CREATE TABLE IF NOT EXISTS ")
-                .append("\"").append(creator.getTableName()).append("\"")
-                .append(" (").append(primaryKeyColumnName).append(" ").append(primaryKeyColumnType.name())
-                .append(" PRIMARY KEY ");
-
-        if (shouldAutoincrement) {
-            sql.append("AUTOINCREMENT ");
-        }
-
-        for (String columnName : creator.getTableFields()) {
-            if (columnName.equals(primaryKeyColumnName)) {
-                continue;
-            }
-            sql.append(", ").append(columnName).append(" ").append(creator.getType(columnName).name());
-
-            if (!creator.isNullAllowed(columnName)) {
-                sql.append(" NOT NULL");
-            }
-
-            if (creator.isUnique(columnName)) {
-                sql.append(" UNIQUE");
-            }
-
-            if (creator.onConflict(columnName) != null && creator.isUnique(columnName)) {
-                sql.append(" ON CONFLICT ").append(creator.onConflict(columnName));
-            }
-        }
-
-        sql.append(");");
-        return sql.toString();
+        return new StatementGenerator().getCreateStatement(creator);
     }
 }
