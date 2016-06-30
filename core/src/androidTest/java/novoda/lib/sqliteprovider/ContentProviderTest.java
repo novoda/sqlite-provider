360e1ecb38e5ae45de5ee3ece22ad8b9dcf902e3
Eduard Bolos <eduard@novoda.com>
2016-06-30 17:25:23 +0100

    Add test to showcase broken insert behaviour

--- core/src/androidTest/java/novoda/lib/sqliteprovider/ContentProviderTest.java
+++ core/src/androidTest/java/novoda/lib/sqliteprovider/ContentProviderTest.java
@@ -1,11 +1,21 @@
 
 package novoda.lib.sqliteprovider;
 
+import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.test.AndroidTestCase;
 
+import java.io.IOException;
+
+import novoda.lib.sqliteprovider.sqlite.ExtendedSQLiteOpenHelper;
+
 public class ContentProviderTest extends AndroidTestCase {
+
+    private static final String TABLE_NAME = "test";
+    private static final String COLUMN_PRIMARY_KEY = "column_primary_key";
+    private static final String COLUMN2 = "column2";
 
     public ContentProviderTest() {
         super();
@@ -17,5 +27,47 @@ public class ContentProviderTest extends AndroidTestCase {
                 null, null, null);
         assertTrue(cursor.getColumnIndexOrThrow("childs__id") > -1);
         assertTrue(cursor.getColumnIndexOrThrow("parents_name") > -1);
+    }
+
+    public void test_GivenATableWithData_When_UpdatingNonexistentRow_Then_NumberOfAffectedRowsShouldBeZero() throws Exception {
+        givenATableWithData();
+
+        ContentValues values = new ContentValues(1);
+        values.put(COLUMN2, 1);
+
+        int numRows = new ExtendedSQLiteOpenHelper(getContext())
+                .getWritableDatabase()
+                .update(TABLE_NAME, values, COLUMN_PRIMARY_KEY + "=?", new String[]{"2"});
+
+        assertEquals(0, numRows);
+
+        numRows = getContext().getContentResolver().update(
+                Uri.parse("content://novoda.lib.sqliteprovider.test/" + TABLE_NAME),
+                values,
+                COLUMN_PRIMARY_KEY + "=?",
+                new String[]{"2"});
+
+        assertEquals(0, numRows);
+    }
+
+    private void givenATableWithData() throws IOException {
+        ExtendedSQLiteOpenHelper helper = new ExtendedSQLiteOpenHelper(getContext());
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.execSQL("CREATE TABLE IF NOT EXISTS `" + TABLE_NAME + "` (" +
+                           "`" + COLUMN_PRIMARY_KEY + "` INTEGER PRIMARY KEY," +
+                           "`" + COLUMN2 + "` INTEGER UNIQUE)");
+
+        ContentValues values = new ContentValues(2);
+        values.put(COLUMN_PRIMARY_KEY, 1);
+        values.put(COLUMN2, 1);
+        helper.getWritableDatabase().insert(TABLE_NAME, null, values);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        ExtendedSQLiteOpenHelper helper = new ExtendedSQLiteOpenHelper(getContext());
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS `" + TABLE_NAME + "`");
     }
 }
