fed7554fcfc007eb31fe7c536d932388aa668507
Eduard Bolos <eduard@novoda.com>
2017-01-03 19:34:34 +0100

    Refactor methods in DatabaseStructure

--- core/src/main/java/novoda/lib/sqliteprovider/util/DatabaseStructure.java
+++ core/src/main/java/novoda/lib/sqliteprovider/util/DatabaseStructure.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import novoda.lib.sqliteprovider.sqlite.IDatabaseMetaInfo.SQLiteType;
 
@@ -17,7 +18,7 @@ public class DatabaseStructure {
     private static final String PRAGMA_TABLE_INFO = "PRAGMA table_info('%1$s');";
     private static final String PRGAMA_INDEX_LIST = "PRAGMA index_list('%1$s');";
     private static final String PRGAMA_INDEX_INFO = "PRAGMA index_info('%1$s');";
-    private static final String COLUMN_PRIMARY_KEY = "pk";
+    private static final String COLUMN_PRIMARY_KEY_INDEX = "pk";
     private static final String COLUMN_NAME = "name";
     private static final String COLUMN_TYPE = "type";
 
@@ -25,132 +26,190 @@ public class DatabaseStructure {
 
     private final SQLiteDatabase database;
 
+    private List<String> createdTables;
+    private Map<String, List<String>> foreignTables = new TreeMap<>();
+    private Map<String, List<Constraint>> uniqueConstraints = new TreeMap<>();
+
     public DatabaseStructure(SQLiteDatabase database) {
         this.database = database;
     }
 
-    /**
-     * @return a list of tables
-     */
-    public List<String> getTables() {
-        final Cursor cur = database.rawQuery(SELECT_TABLES_NAME, null);
-        List<String> createdTable = new ArrayList<>(cur.getCount());
-        String tableName;
-        while (cur.moveToNext()) {
-            tableName = cur.getString(0);
-            if (!DEFAULT_TABLES.contains(tableName)) {
-                createdTable.add(tableName);
-            }
-        }
-        cur.close();
-        return Collections.unmodifiableList(createdTable);
+    public List<String> tables() {
+        final Cursor tablesCursor = database.rawQuery(SELECT_TABLES_NAME, null);
+        createdTables = new ArrayList<>(tablesCursor.getCount());
+        parseTables(tablesCursor);
+        tablesCursor.close();
+        return Collections.unmodifiableList(createdTables);
     }
 
-    public Map<String, String> getProjectionMap(String parent, String... foreignTables) {
-        Map<String, String> projection = new HashMap<>();
+    private void parseTables(Cursor tablesCursor) {
+        while (tablesCursor.moveToNext()) {
+            addCreatedTableIfNotDefault(tablesCursor.getString(0));
+        }
+    }
+
+    private void addCreatedTableIfNotDefault(String tableName) {
+        if (!DEFAULT_TABLES.contains(tableName)) {
+            createdTables.add(tableName);
+        }
+    }
+
+    public Map<String, String> projectionMap(String parent, String... foreignTables) {
+        Map<String, String> projection = new TreeMap<>();
         projection.put("_id", parent + "._id AS _id");
-        for (Map.Entry<String, SQLiteType> entry : getFields(parent).entrySet()) {
-            projection.put(parent + "_" + entry.getKey(), parent + "." + entry.getKey() + " AS "
-                    + parent + "_" + entry.getKey());
-        }
-        for (String ft : foreignTables) {
-            for (Map.Entry<String, SQLiteType> entry : getFields(ft).entrySet()) {
-                projection.put(ft + "_" + entry.getKey(), ft + "." + entry.getKey() + " AS " + ft
-                        + "_" + entry.getKey());
-            }
-        }
+        projection.putAll(projectionFor(parent, columns(parent)));
+        projection.putAll(projectionFor(foreignTables));
         return Collections.unmodifiableMap(projection);
     }
 
-    public List<String> getForeignTables(String table) {
-        final Cursor cur = queryTableColumnsFor(table);
-        List<String> tables = getTables();
-        List<String> foreignTables = new ArrayList<>(5);
-        String name;
-        String tableName;
-        while (cur.moveToNext()) {
-            name = cur.getString(cur.getColumnIndexOrThrow(COLUMN_NAME));
-            if (name.endsWith("_id")) {
-                tableName = name.substring(0, name.lastIndexOf('_'));
-                if (tables.contains(tableName + "s")) {
-                    foreignTables.add(tableName + "s");
-                } else if (tables.contains(tableName)) {
-                    foreignTables.add(tableName);
-                }
-            }
+    private static Map<String, String> projectionFor(String table, Map<String, SQLiteType> fields) {
+        Map<String, String> projection = new TreeMap<>();
+        for (Map.Entry<String, SQLiteType> entry : fields.entrySet()) {
+            projection.put(table + "_" + entry.getKey(), table + "." + entry.getKey() + " AS "
+                    + table + "_" + entry.getKey());
         }
-        cur.close();
-        return Collections.unmodifiableList(foreignTables);
-
+        return projection;
     }
 
-    public Map<String, SQLiteType> getFields(String table) {
-        final Cursor cursor = queryTableColumnsFor(table);
-        Map<String, SQLiteType> fields = new HashMap<>(cursor.getCount());
-        String name;
-        String type;
-        while (cursor.moveToNext()) {
-            name = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME));
-            type = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TYPE));
-            fields.put(name, SQLiteType.fromName(type));
+    private Map<String, String> projectionFor(String... foreignTables) {
+        Map<String, String> projection = new TreeMap<>();
+        for (String ft : foreignTables) {
+            projection.putAll(projectionFor(ft, columns(ft)));
         }
-        cursor.close();
-        return Collections.unmodifiableMap(fields);
+        return projection;
+    }
+
+    public List<String> foreignTables(String table) {
+        foreignTables.put(table, new ArrayList<String>(5));
+        final Cursor columnsCursor = queryTableColumnsFor(table);
+        List<String> allTables = tables();
+        parseForeignTablesFor(table, columnsCursor, allTables);
+        columnsCursor.close();
+        return Collections.unmodifiableList(foreignTables.get(table));
+    }
+
+    private void parseForeignTablesFor(String table, Cursor columnsCursor, List<String> allTables) {
+        while (columnsCursor.moveToNext()) {
+            String columnName = columnsCursor.getString(columnsCursor.getColumnIndexOrThrow(COLUMN_NAME));
+            addForeignTableIfExists(table, columnName, allTables);
+        }
+    }
+
+    private void addForeignTableIfExists(String forTable, String columnName, List<String> tables) {
+        if (!isForeignKey(columnName)) {
+            return;
+        }
+        String tableName = columnName.substring(0, columnName.lastIndexOf('_'));
+        if (tables.contains(tableName + "s")) {
+            foreignTables.get(forTable).add(tableName + "s");
+        } else if (tables.contains(tableName)) {
+            foreignTables.get(forTable).add(tableName);
+        }
+    }
+
+    private boolean isForeignKey(String columnName) {
+        return columnName.endsWith("_id");
+    }
+
+    public Map<String, SQLiteType> columns(String table) {
+        final Cursor columnsCursor = queryTableColumnsFor(table);
+        Map<String, SQLiteType> columns = parseColumns(columnsCursor);
+        columnsCursor.close();
+        return Collections.unmodifiableMap(columns);
+    }
+
+    private Map<String, SQLiteType> parseColumns(Cursor columnsCursor) {
+        Map<String, SQLiteType> columns = new HashMap<>(columnsCursor.getCount());
+        while (columnsCursor.moveToNext()) {
+            String name = columnsCursor.getString(columnsCursor.getColumnIndexOrThrow(COLUMN_NAME));
+            String type = columnsCursor.getString(columnsCursor.getColumnIndexOrThrow(COLUMN_TYPE));
+            columns.put(name, SQLiteType.fromName(type));
+        }
+        return columns;
     }
 
     private Cursor queryTableColumnsFor(String table) {
         return database.rawQuery(String.format(PRAGMA_TABLE_INFO, table), null);
     }
 
-    public List<Constraint> getUniqueConstraints(String table) {
-        List<Constraint> constraints = new ArrayList<>();
+    public List<Constraint> uniqueConstraints(String table) {
+        uniqueConstraints.put(table, new ArrayList<Constraint>());
+        lookForImplicitUniqueIndex(table);
+        final Cursor indexCursor = queryIndexListForTable(table);
+        parseUniqueConstraints(indexCursor, table);
+        indexCursor.close();
+        return Collections.unmodifiableList(uniqueConstraints.get(table));
+    }
 
+    private void lookForImplicitUniqueIndex(String table) {
         // This is an implicit unique index, and won't show up querying the other indexes
         Constraint integerPrimaryKeyConstraint = findIntegerPrimaryKeyConstraint(table);
         if (integerPrimaryKeyConstraint != null) {
-            constraints.add(integerPrimaryKeyConstraint);
+            uniqueConstraints.get(table).add(integerPrimaryKeyConstraint);
         }
+    }
 
-        final Cursor indexCursor = queryIndexListForTable(table);
+    private void parseUniqueConstraints(Cursor indexCursor, String forTable) {
         while (indexCursor.moveToNext()) {
-            int isUnique = indexCursor.getInt(2);
-            if (isUnique == 1) {
-                String indexName = indexCursor.getString(1);
-                final Cursor columnCursor = queryIndexInfo(indexName);
-                List<String> columns = new ArrayList<>(columnCursor.getCount());
-                while (columnCursor.moveToNext()) {
-                    String columnName = columnCursor.getString(2);
-                    columns.add(columnName);
-                }
-                columnCursor.close();
-                constraints.add(new Constraint(columns));
-            }
+            addUniqueIndexConstraint(indexCursor, forTable);
         }
-        indexCursor.close();
-        return constraints;
+    }
+
+    private void addUniqueIndexConstraint(Cursor indexCursor, String forTable) {
+        if (!isIndexUnique(indexCursor)) {
+            return;
+        }
+        String indexName = indexCursor.getString(1);
+        final Cursor indexInfoCursor = queryIndexInfo(indexName);
+        List<String> columns = parseIndexColumns(indexInfoCursor);
+        indexInfoCursor.close();
+        uniqueConstraints.get(forTable).add(new Constraint(columns));
+    }
+
+    private boolean isIndexUnique(Cursor indexCursor) {
+        return indexCursor.getInt(2) == 1;
+    }
+
+    private List<String> parseIndexColumns(Cursor indexInfoCursor) {
+        List<String> columns = new ArrayList<>(indexInfoCursor.getCount());
+        while (indexInfoCursor.moveToNext()) {
+            String columnName = indexInfoCursor.getString(2);
+            columns.add(columnName);
+        }
+        return columns;
     }
 
     private Constraint findIntegerPrimaryKeyConstraint(String table) {
-        final Cursor cursor = queryTableColumnsFor(table);
+        final Cursor columnsCursor = queryTableColumnsFor(table);
         try {
-            while (cursor.moveToNext()) {
-                if (isTableInfoItemAnIntegerPrimaryKey(cursor)) {
-                    String columnName = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
-                    return new Constraint(Collections.singletonList(columnName));
-                }
-            }
+            return parseColumnsForIntegerPrimaryKeyConstraint(columnsCursor);
         } finally {
-            cursor.close();
+            columnsCursor.close();
+        }
+    }
+
+    private Constraint parseColumnsForIntegerPrimaryKeyConstraint(Cursor columnsCursor) {
+        Constraint integerPrimaryKeyConstraint = null;
+        while (columnsCursor.moveToNext() && integerPrimaryKeyConstraint == null) {
+            integerPrimaryKeyConstraint = extractIntegerPrimaryKeyConstraintFromColumn(columnsCursor);
+        }
+        return integerPrimaryKeyConstraint;
+    }
+
+    private Constraint extractIntegerPrimaryKeyConstraintFromColumn(Cursor columnsCursor) {
+        if (isTableInfoItemAnIntegerPrimaryKey(columnsCursor)) {
+            String columnName = columnsCursor.getString(columnsCursor.getColumnIndex(COLUMN_NAME));
+            return new Constraint(Collections.singletonList(columnName));
         }
         return null;
     }
 
     private static boolean isTableInfoItemAnIntegerPrimaryKey(Cursor cursor) {
-        int pkInt = cursor.getInt(cursor.getColumnIndex(COLUMN_PRIMARY_KEY));
+        int primaryKeyIndex = cursor.getInt(cursor.getColumnIndex(COLUMN_PRIMARY_KEY_INDEX));
         String columnType = cursor.getString(cursor.getColumnIndex(COLUMN_TYPE));
 
         // NOT A BOOLEAN AS INTEGER: 0 if not part of PK, otherwise index of column in key: https://www.sqlite.org/pragma.html#pragma_table_info
-        boolean isPrimaryKey = pkInt != 0;
+        boolean isPrimaryKey = primaryKeyIndex != 0;
         boolean isInteger = SQLiteType.fromName(columnType) == SQLiteType.INTEGER;
 
         return isPrimaryKey && isInteger;
